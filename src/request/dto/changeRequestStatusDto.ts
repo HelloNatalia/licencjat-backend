8e10b08707bf7094603f0c88939adc961c7f2a47
unknown <nataliatucka01@gmail.com>
2024-02-26 15:13:14 +0100

    Changed DTO

--- src/request/dto/changeRequestStatusDto.ts
+++ src/request/dto/changeRequestStatusDto.ts
@@ -1,7 +1,8 @@
-import { IsEnum } from '@nestjs/class-validator';
+import { IsEnum, IsString } from '@nestjs/class-validator';
 import { Status } from '../status.enum';
 import { Transform } from 'class-transformer';
 
 export class ChangeRequestStatusDto {
+  @IsString()
   status: string;
 }
