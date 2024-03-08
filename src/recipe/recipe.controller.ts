fd7602230697ca696f80008e7019bb3989573d69
unknown <nataliatucka01@gmail.com>
2024-03-08 13:42:44 +0100

    Adding recipe category to database

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { CreateRecipeCategoryDto } from './dto/createRecipeCategoryDto';
 
 @Controller('recipe')
-export class RecipeController {}
+export class RecipeController {
+  constructor(private recipeService: RecipeService) {}
+
+  @Post('create-recipe-category')
+  createRecipeCategory(
+    @Body() createRecipeCategoryDto: CreateRecipeCategoryDto,
+  ): Promise<void> {
+    return this.recipeService.createRecipeCategory(createRecipeCategoryDto);
+  }
+}
--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Recipe } from './recipe.entity';
+import { Repository } from 'typeorm';
+import { RecipeCategory } from './recipe-category.entity';
+import { RecipeProduct } from './recipe-product.entity';
+import { CreateRecipeCategoryDto } from './dto/createRecipeCategoryDto';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+  constructor(
+    @InjectRepository(Recipe)
+    private recipeRepository: Repository<Recipe>,
+
+    @InjectRepository(RecipeCategory)
+    private recipeCategoryRepository: Repository<RecipeCategory>,
+
+    @InjectRepository(RecipeProduct)
+    private recipeProductRepository: Repository<RecipeProduct>,
+  ) {}
+
+  async createRecipeCategory(
+    createRecipeCategoryDto: CreateRecipeCategoryDto,
+  ): Promise<void> {
+    const { name, description } = createRecipeCategoryDto;
+
+    const recipeCategory = this.recipeCategoryRepository.create({
+      name,
+      description,
+    });
+
+    try {
+      await this.recipeCategoryRepository.save(recipeCategory);
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Something went wrong');
+    }
+  }
+}
