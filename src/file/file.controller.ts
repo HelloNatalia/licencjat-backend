54b8b0c9fd68c275922cfeb0c0ea7a7fd2bd5025
unknown <nataliatucka01@gmail.com>
2024-04-09 20:01:03 +0200

    Getting announcements photos

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,11 +1,19 @@
 import {
   Controller,
+  Get,
+  NotFoundException,
+  Param,
   Post,
+  Req,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const UPLOADS_DIR = path.resolve(__dirname, '..', 'uploads');
 
 @Controller('file')
 export class FileController {
@@ -13,7 +21,7 @@ export class FileController {
   @UseInterceptors(
     FilesInterceptor('photos', 10, {
       storage: diskStorage({
-        destination: './uploads',
+        destination: UPLOADS_DIR,
         filename: (req, file, cb) => {
           cb(null, file.originalname);
         },
@@ -22,5 +30,20 @@ export class FileController {
   )
   async uploadPhotos(@UploadedFiles() photos: Express.Multer.File[]) {
     return { photos };
+  }
+
+  @Get(':fileName')
+  async getPhoto(@Param('fileName') fileName: string, @Req() req: any) {
+    const baseUrl = req.protocol + '://' + req.get('host'); // pobierz bazowy URL z żądania
+    const fileUrl = baseUrl + '/uploads/' + fileName; // zbuduj pełny URL pliku
+
+    const filePath = path.join(__dirname, '..', 'uploads', fileName);
+    const fileExists = fs.existsSync(filePath);
+
+    if (!fileExists) {
+      throw new NotFoundException('File not found');
+    }
+
+    return { url: fileUrl }; // zwróć pełny URL pliku
   }
 }
--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './uploads',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
     }),
   ],
   controllers: [FileController],
